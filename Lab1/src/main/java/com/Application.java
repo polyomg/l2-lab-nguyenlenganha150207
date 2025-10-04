cae846434f0ab1e792e0ba7f4e165735029aa018
Nguyen Le Ngan Ha <nguyenlenganha923@gmail.com>
2025-10-04 11:29:59 +0700

    first commit

--- Lab1/src/main/java/com/Application.java
+++ Lab1/src/main/java/com/Application.java
@@ -1,13 +1,11 @@
-package com.poly.lab1;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab1Application {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Lab1Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
