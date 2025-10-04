eceeb960e0ee47bba48ada9fb2641160f2bd597d
Nguyen Le Ngan Ha <nguyenlenganha923@gmail.com>
2025-10-04 11:37:20 +0700

    TS01135_NguyenLeNganHa_Lab3

--- Lab3/src/main/java/com/Application.java
+++ Lab3/src/main/java/com/Application.java
@@ -1,13 +1,11 @@
-package com.poly.lab3;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab3Application {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Lab3Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
